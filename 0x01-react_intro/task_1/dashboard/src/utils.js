4166fcf1b0960ffc980a2a4abb0f0566874fc69d
jhay <omalejoy83@gmail.com>
2024-05-09 01:28:52 +0100

    task 2 done

---
+++ 0x01-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,7 @@
+// utils.js
+
+const getFullYear = () => new Date().getFullYear();
+
+const getFooterCopy = (isIndex) => (isIndex ? 'Holberton School' : 'Holberton School main dashboard');
+
+export { getFullYear, getFooterCopy };
